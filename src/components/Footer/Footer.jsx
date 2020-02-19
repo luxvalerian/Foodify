1f42a489fe8ed8f7d93ab252917bd0d0ec470233
Avelynn Mitra <avelynnmitra@gmail.com>
2020-02-18 19:31:57 -0600

    adds navbar components

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Footer.module.css';
+
+const Footer = (props) => {
+    return (
+        <footer className={styles.footer}>
+            <h3>Copyright Foodify 2020</h3>
+        </footer>
+    );
+}
+
+export default Navbar;
